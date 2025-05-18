1971168d7675b8b9b3bdcff166aaafd7ddcba38d
rk28284 <112754760+rk28284@users.noreply.github.com>
2025-05-18 23:32:38 +0530

    Backend completed

--- model/complaint.model.js
+++ model/complaint.model.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 const complaintSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+
   title: { type: String, required: true },
   description: { type: String, required: true },
   imageUrl: { type: String },
--- route/complaint.routes.js
+++ route/complaint.routes.js
@@ -1,20 +1,19 @@
 const express = require('express');
-const myComlaintRouter = express.Router();
+const myComplaintRouter = express.Router();
+
 const {
-getComplaint,getSingleComplaint,updateComplaint,addComplaint
+  getComplaint,
+  getSingleComplaint,
+  updateComplaint,
+  addComplaint
 } = require('../controllers/complaint.controller');
+
 const authmiddleware = require('../middleware/auth.middleware');
 
-// GET /api/mybooks
-myComlaintRouter.get('/', getComplaint);
+// Routes with authmiddleware to access req.user
+myComplaintRouter.get('/', getComplaint);
+myComplaintRouter.post('/add', addComplaint);
+myComplaintRouter.patch('/:Id/status', updateComplaint);
+myComplaintRouter.get('/:Id', getSingleComplaint);
 
-// POST /api/mybooks/:bookId
-myComlaintRouter.post('/add', addComplaint);
-
-// PATCH /api/mybooks/:bookId/status
-myComlaintRouter.patch('/:Id/status', updateComplaint);
-
-// PATCH /api/mybooks/:bookId/rating
-myComlaintRouter.patch('/:Id',getSingleComplaint );
-
-module.exports = myComlaintRouter;
+module.exports = myComplaintRouter;
